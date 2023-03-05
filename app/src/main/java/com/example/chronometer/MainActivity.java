233d6580d40c4952ad1ae1b789b014c424b7c55d
Francesco Antoni <francesco.antoni09@gmail.com>
2023-03-05 13:11:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/chronometer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.chronometer;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.chronometer.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+}
